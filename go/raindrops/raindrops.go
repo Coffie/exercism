72b40f2c7e02a4d0e58ba6c90bb789bd0391baaf
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2019-06-05 14:09:13 +0200

    add raindrops solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
-func Convert(number int) string {
+import (
+	"fmt"
+)
 
+func Convert(number int) string {
+	factors := []int{3,5,7}
+	var result string
+	factorMap := map[int]string{
+		3: "Pling",
+		5: "Plang",
+		7: "Plong",
+	}
+	for _, v := range factors {
+		if number % v == 0 {
+			result = fmt.Sprintf("%s%s", result, factorMap[v])
+		}
+	}
+	if result == "" {
+		result = fmt.Sprintf("%d", number)
+	}
+	return result
 }
+
