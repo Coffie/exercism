3d653e65bc1fa176b68f14c4d6cd8fb0674fc6fa
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2019-06-07 09:33:38 +0200

    refactor functions

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,15 +2,13 @@
 package diffsquares
 
 // SquareOfSum calculates the square of sum for all natural numbers from 1 to input, n.
-func SquareOfSum(n int) (sum int) {
-	sum = (n * (n + 1))/2
-	return sum*sum
+func SquareOfSum(n int) int {
+	return n * n * (n + 1) * (n + 1) / 4
 }
 
 // SumOfSquares calculates the sum of squares for all natural numbers from 1 to input, n.
-func SumOfSquares(n int) (sum int) {
-	sum = (n * (n + 1) * (2 * n + 1)) / 6
-	return sum
+func SumOfSquares(n int) int {
+	return n * (n + 1) * (2 * n + 1) / 6
 }
 
 // Difference returns the difference between the square of sum and sum of squares.
