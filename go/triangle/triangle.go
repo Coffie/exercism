ca234b50d62c69e544800f477399788eac09d491
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2020-01-13 13:21:33 +0530

    Add triangle solution

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,32 +1,40 @@
 package triangle
 
+import (
+	"math"
+)
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
 type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT = 0
-    Equ = 1
-    Iso = 2
-    Sca = 3
+    NaT = iota
+    Equ
+    Iso
+    Sca
 )
 
-// KindFromSides should have a comment documenting it.
+// KindFromSides gets three values and evaluates which kind of triangle it makes
 func KindFromSides(a, b, c float64) Kind {
 	var k Kind
-	input := [3]float64{a,b,c}
-	sides := make(map[float64]int)
-	for side := range input {
-		if _, ok := sides[input[side]]; ok {
-			sides[input[side]] += 1
-		} else {
-			sides[input[side]] = 1
-		}
-	}
-	if _, ok := sides[0.0]; ok {
+	ab, ac, bc := a + b, a + c, b + c
+	if invalidNumber(a) || invalidNumber(b) || invalidNumber(c) {
 		k = NaT
+	} else if ab < c || ac < b || bc < a {
+		k = NaT
+	} else if a == b && b == c {
+		k = Equ
+	} else if a == b || a == c || b == c{
+		k = Iso
+	} else if a != b && b != c {
+		k = Sca
 	}
-	
 	return k
+}
+
+// invalidNumber checks if a value is a valid side of a triangle
+func invalidNumber(num float64) bool {
+	if math.IsNaN(num) || math.IsInf(num, 1) || math.IsInf(num, -1) || num == 0.0 {
+		return true
+	}
+	return false
 }
