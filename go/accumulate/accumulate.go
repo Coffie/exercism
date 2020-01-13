97b2eb94eb1b4732fbb0c8439cfe53b9b09c04ed
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2020-01-13 15:46:10 +0530

    Add accumulate solution

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,1 +1,10 @@
 package accumulate
+
+// Accumulate takes a list of strings and applies a function to each value.
+func Accumulate(in []string, converter func(string) string) []string {
+	out := make([]string, len(in))
+	for i, val := range in {
+		out[i] = converter(val)
+	}
+	return out
+}
