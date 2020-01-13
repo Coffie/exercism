2244ac0653cfd00f077501ea5733bdb7c8987371
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2020-01-13 13:40:33 +0530

    Refactor code for readability

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,8 +1,3 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
 
 import "fmt"
@@ -12,27 +7,16 @@ const (
 	FinalPhrase = "And all for the want of a %s."
 )
 
-// Proverb should have a comment documenting it.
+// Proverb builds a poem given a list of proverbs
 func Proverb(rhyme []string) []string {
-	var poem []string
-	var endPhrase string
 	length := len(rhyme)
-	if length > 0 {
-		endPhrase = fmt.Sprintf(FinalPhrase, rhyme[0])
+	if length == 0 {
+		return nil
 	}
-	switch length {
-	case 0:
-		return poem
-	case 1:
-		return append(poem, endPhrase)
-	default:
-		for i, r := range rhyme {
-			if i == length - 2 {
-				poem = append(poem, fmt.Sprintf(DefaultPhrase, r, rhyme[i+1]))
-				break
-			}
-			poem = append(poem, fmt.Sprintf(DefaultPhrase, r, rhyme[i+1]))
-		}
+	poem := make([]string, length)
+	poem[length-1] = fmt.Sprintf(FinalPhrase, rhyme[0])
+	for i := 0; i < length - 1; i++ {
+		poem[i] = fmt.Sprintf(DefaultPhrase, rhyme[i], rhyme[i+1])
 	}
-	return append(poem, endPhrase)
+	return poem
 }
