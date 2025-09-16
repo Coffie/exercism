39eac470e0995a7439033a729f0116641c36057a
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2025-09-16 10:44:45 +0200

    add python solutions

---
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,52 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language:
+https://en.wikipedia.org/wiki/Guido_van_Rossum
+
+This is a module docstring, used to describe the functionality
+of a module and its functions and/or classes.
+"""
+
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+
+
+def bake_time_remaining(elapsed_bake_time):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+
+def preparation_time_in_minutes(number_of_layers):
+    """Calculate the preparation time in minutes.
+
+    :param number_of_layers: int - number of layers desired for the lasagna.
+    :return: int - time in minutes to prepare the desired amount of layers based
+    on the `PREPARATION_TIME`.
+
+    This function takes an integer representing the number of layers in the lasagna
+    and calculates the preparation time in minutes to prepare the lasagna.
+    """
+    return PREPARATION_TIME * number_of_layers
+
+
+# TODO: define the 'elapsed_time_in_minutes()' function below.
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate the total minutes in the kitchen cooking.
+
+    :param number_of_layers: int - number of layers desired for the lasagna.
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - total time in minutes spent in the kitchen cooking.
+
+    This function takes two integers representing the number of lasagna layers and the
+    time already spent baking and calculates the total elapsed minutes spent cooking the
+    lasagna.
+    """
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
---
+++ python/inventory-management/dicts.py
@@ -0,0 +1,59 @@
+"""Functions to keep track and alter inventory."""
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+    return {item: items.count(item) for item in set(items)}
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+    for item in items:
+        inventory[item] = inventory.get(item, 0) + 1
+    return inventory
+
+
+def decrement_items(inventory, items, inventory_limit=0):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+    for item in items:
+        if inventory.get(item, 0) > 0:
+            inventory[item] -= 1
+        # Allows 0 inventory and negative inventory based on limit
+        # inventory[item] = max(inventory_limit, inventory.get(item, 0) - 1)
+    return inventory
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+    inventory.pop(item, "not found")
+    return inventory
+
+
+def list_inventory(inventory):
+    """Create a list containing only available (item_name, item_count > 0) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+    # functional style
+    # return list(filter(lambda kv: kv[1] > 0, inventory.items()))
+    return [(key, value) for key, value in inventory.items() if value > 0]
